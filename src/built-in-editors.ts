81e8b3ca38fe866c22b4317780ce92111994fcd0
nienow <system@randombits.dev>
2023-04-20 17:26:27 -0500

    Added all standard notes built-in editors

--- src/built-in-editors.ts
+++ src/built-in-editors.ts
@@ -15,6 +15,66 @@ export const BUILT_IN_EDITORS: Editor[] = [
     desc: ''
   },
   {
+    id: 'simple-markdown',
+    url: 'https://app.standardnotes.com/components/assets/org.standardnotes.simple-markdown-editor/dist/index.html',
+    name: 'Simple Markdown',
+    desc: ''
+  },
+  {
+    id: 'advanced-markdown',
+    url: 'https://app.standardnotes.com/components/assets/org.standardnotes.advanced-markdown-editor/dist/index.html',
+    name: 'Advanced markdown',
+    desc: ''
+  },
+  {
+    id: 'visual-markdown',
+    url: 'https://app.standardnotes.com/components/assets/org.standardnotes.markdown-visual-editor/build/index.html',
+    name: 'Alternative Markdown',
+    desc: ''
+  },
+  {
+    id: 'minimal-markdown',
+    url: 'https://app.standardnotes.com/components/assets/org.standardnotes.minimal-markdown-editor/index.html',
+    name: 'Minimal Markdown',
+    desc: ''
+  },
+  {
+    id: 'tasks',
+    url: 'https://app.standardnotes.com/components/assets/org.standardnotes.simple-task-editor/dist/index.html',
+    name: 'Simple Task',
+    desc: ''
+  },
+  {
+    id: 'code',
+    url: 'https://app.standardnotes.com/components/assets/org.standardnotes.code-editor/index.html',
+    name: 'Code',
+    desc: ''
+  },
+  {
+    id: 'bold',
+    url: 'https://app.standardnotes.com/components/assets/org.standardnotes.bold-editor/dist/index.html',
+    name: 'Bold',
+    desc: ''
+  },
+  {
+    id: 'sheets',
+    url: 'https://app.standardnotes.com/components/assets/org.standardnotes.standard-sheets/dist/index.html',
+    name: 'Sheets',
+    desc: '',
+  },
+  {
+    id: 'math',
+    url: 'https://app.standardnotes.com/components/assets/org.standardnotes.fancy-markdown-editor/index.html',
+    name: 'Math markdown',
+    desc: ''
+  },
+  {
+    id: 'plus',
+    url: 'https://app.standardnotes.com/components/assets/org.standardnotes.plus-editor/dist/index.html',
+    name: 'Rich Text Plus',
+    desc: ''
+  },
+  {
     id: 'spreadsheet',
     name: 'Spreadsheet',
     desc: 'Spreadsheet'
