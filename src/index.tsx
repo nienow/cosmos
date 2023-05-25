76232523d4ba9062459b49e4629d8004ac6ad458
nienow <system@randombits.dev>
2023-05-25 08:16:48 -0500

    Removed mobile debugging

--- src/index.tsx
+++ src/index.tsx
@@ -8,11 +8,6 @@ import {DialogProvider} from "./providers/DialogProvider";
 
 setup(React.createElement);
 
-window.addEventListener('error', (e) => {
-  console.log(e);
-  document.getElementById('errors').innerText += '\n' + e.message;
-});
-
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <DialogProvider>
